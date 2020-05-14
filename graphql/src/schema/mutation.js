f03f0a2ca66eb17d9d8abb0276506395fdfed4a4
Sarah Ting (macbook) <sarah.j.ting@gmail.com>
2020-05-14 15:02:05 +0900

    add wheel spinning to graphQL schema

--- graphql/src/schema/mutation.js
+++ graphql/src/schema/mutation.js
@@ -2,10 +2,11 @@ const {
   GraphQLObjectType,
   GraphQLInt,
   GraphQLFloat,
-  GraphQLNonNull
+  GraphQLNonNull,
 } = require('graphql');
 const axios = require('axios');
 const DiceBet = require('./dice-bet');
+const WheelBet = require('./wheel-bet');
 const Seed = require('./seed');
 
 exports.Type = new GraphQLObjectType({
@@ -15,27 +16,52 @@ exports.Type = new GraphQLObjectType({
       type: DiceBet.Type,
       args: {
         target: { type: GraphQLNonNull(GraphQLInt) },
-        amount: { type: GraphQLNonNull(GraphQLFloat) }
+        amount: { type: GraphQLNonNull(GraphQLFloat) },
       },
       async resolve(parent, { amount, target }, { user }) {
         const { data } = await axios.post(`http://dice/roll-dice`, {
           user,
           amount,
-          target
+          target,
         });
         return data;
-      }
+      },
     },
 
     rotateDiceSeed: {
       type: Seed.Type,
       async resolve(parent, args, { user }) {
         const { data } = await axios.post(`http://dice/rotate-seed`, {
-          user
+          user,
         });
 
         return data;
-      }
-    }
-  })
+      },
+    },
+
+    spinWheel: {
+      type: WheelBet.Type,
+      args: {
+        amount: { type: GraphQLNonNull(GraphQLFloat) },
+      },
+      async resolve(_, { amount }, { user }) {
+        const { data } = await axios.post(`http://wheel/spin-wheel`, {
+          user,
+          amount,
+        });
+        return data;
+      },
+    },
+
+    rotateWheelSeed: {
+      type: Seed.Type,
+      async resolve(_, __, { user }) {
+        const { data } = await axios.post(`http://wheel/rotate-seed`, {
+          user,
+        });
+
+        return data;
+      },
+    },
+  }),
 });
